79ee57280f89a50d29f6ab3d8a2e977eea96015b
Luiz Henrique <lmenezesmessias@gmail.com>
2021-05-29 22:40:04 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, z = 0, x = 1, y = 0;
+            bool valor;
+
+            Console.WriteLine("\nSequência de Fibonacci\n");
+
+            Console.Write("Quantos termos (>= 2)? ");
+            valor = Int32.TryParse(Console.ReadLine(), out numero);
+
+            for (int finish = 0; finish < numero; finish++)
+            {
+                z = x + y;
+                Console.WriteLine(z);
+                y = x;
+                x = z;
+            }
+            if (numero < 2 || !valor)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Número invalido.");
+                Console.ResetColor();
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
